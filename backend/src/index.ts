4474eb21c0386d983ae70524496122f5d46fb9ae
Devang kushwah <deekayedm@gmail.com>
2024-04-08 13:27:18 +0530

    prisma schema

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,35 +1,40 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client';
+import { withAccelerate } from '@prisma/extension-accelerate';
 
-const app = new Hono()
-
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL : string
+  }
+}>();
 
 app.post('/api/v1/user/signup', (c) => {
- return c.text("signup")
-})
-
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL
+  }).$extends(withAccelerate())
+  return c.text("signup route")
+});
 
 app.post('/api/v1/user/signin', (c) => {
-  return c.text("signin")
-})
-
+  return c.text("signin route")
+});
 
 app.post('/api/v1/blog', (c) => {
   return c.text("blog")
-})
+});
 
 app.put('/api/v1/blog', (c) => {
   return c.text("updated blog")
-})
+});
 
-app.get('/api/v1/blog:id', (c) => {
-  return c.text("signup")
-})
+app.get('/api/v1/blog/:id', (c) => {
+  const id = c.req.param('id');
+  console.log(id)
+  return c.text("get blog route")
+});
 
 app.get('/api/v1/blog/bulk', (c) => {
-  return c.text("signup")
+  return c.text("bluk route")
 })
 
 export default app
