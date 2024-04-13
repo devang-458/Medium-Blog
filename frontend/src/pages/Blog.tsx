929dd1843715e145913e8fef3bc06aaf67732107
Devang kushwah <deekayedm@gmail.com>
2024-04-13 20:15:51 +0530

    frontEnd done

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -1,15 +1,26 @@
 import { useParams } from "react-router-dom"
 import { useBlog } from "../hooks"
 import { FullBlog } from "../components/FullBlog"
+import { Appbar } from "../components/Appbar"
+import { BlogSkeleton } from "../components/BlogSkeleton"
 
 export const Blog = () => {
-    const {id} = useParams()
-    const {loading , blog} = useBlog({
+    const { id } = useParams()
+    const { loading, blog } = useBlog({
         id: id || ""
     })
-    if(loading){
+    if (loading || !blog) {
         return <div>
-            loading.....
+            <Appbar />
+            <div className="flex flex-row justify-center px-100">
+                <div className="pb-10 ">
+                    <BlogSkeleton />
+                </div>
+                <div>
+                    <BlogSkeleton />
+                </div>
+            </div>
+
         </div>
     }
     return <div>
