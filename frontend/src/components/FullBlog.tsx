e142094b6763f51c05d197a4163c4181248421a6
Devang kushwah <deekayedm@gmail.com>
2024-04-13 22:23:26 +0530

    skeleton added

--- frontend/src/components/FullBlog.tsx
+++ frontend/src/components/FullBlog.tsx
@@ -2,43 +2,43 @@ import { Blog } from "../hooks"
 import { Appbar } from "./Appbar"
 import { Avatar } from "./BlogCard"
 
-export const FullBlog = ({ blog }: {blog: Blog}) => {
-    return <div>
-        <Appbar />
-        <div className="flex justify-center ">
-            <div className="grid grid-cols-12 px-20 w-full pt-200 max-w-screen-xl pt-12">
-                <div className="col-span-8">
-                    <div className="text-5xl font-extrabold">
-                        {blog.title}
-                    </div>
-                    <div className="text-slate-500 pt-2">
-                        Post on 2nd December 2023
-                    </div>
-                    <div className="pt-4 text-xl">
-                        {blog.content}
-                    </div>
-                </div>
-                <div className="col-span-4 ">
-                    <div className="text-slate-600 text-lg rounded-lg ">
-                        Author :-
-                    </div>
-                    <div className="flex w-full">
-                        <div className="p-4 flex flex-col justify-center ">
-                            <Avatar size="big" name={blog.author.name.toUpperCase() || "Anonymous"} />
+export const FullBlog = ({ blog }: { blog: Blog }) => {
+    return (
+        <div>
+            <Appbar />
+            <div className="flex justify-center">
+                <div className="grid grid-cols-1 md:grid-cols-12 gap-4 px-4 md:px-20 w-full pt-12 max-w-screen-xl">
+                    <div className="md:col-span-8">
+                        <div className="text-3xl md:text-5xl font-extrabold">
+                            {blog.title}
                         </div>
-                        <div>
-                            <div className="text-xl text-slate-600 font-bold">
-                                {blog.author.name.toUpperCase() || "Anonymous"}
+                        <div className="text-slate-500 pt-2">
+                            Post on 2nd December 2023
+                        </div>
+                        <div className="pt-4 text-base md:text-xl">
+                            {blog.content}
+                        </div>
+                    </div>
+                    <div className="md:col-span-4">
+                        <div className="text-slate-600 text-lg rounded-lg">
+                            Author:-
+                        </div>
+                        <div className="flex flex-col md:flex-row items-start md:items-center w-full">
+                            <div className="p-6 justify-center hidden md:visible ">
+                                <Avatar size="big" name={blog.author.name.toUpperCase() || "Anonymous"} />
                             </div>
-                            <div className="text-slate-500">
-                            Artistic soul, loves painting and sketching, enjoys gardening, food blogger, and passionate about social justice issues.
-                             </div>
+                            <div className="md:pl-4">
+                                <div className="text-xl text-slate-600 font-bold ">
+                                    {blog.author.name.toUpperCase() || "Anonymous"}
+                                </div>
+                                <div className="text-slate-500 pb-10 bottom">
+                                    Artistic soul, loves painting and sketching, enjoys gardening, food blogger, and passionate about social justice issues.
+                                </div>
+                            </div>
                         </div>
-                    </div>  
+                    </div>
                 </div>
-                
             </div>
         </div>
-    </div>
-}
-
+    );
+};
--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -2,7 +2,7 @@ import { useParams } from "react-router-dom"
 import { useBlog } from "../hooks"
 import { FullBlog } from "../components/FullBlog"
 import { Appbar } from "../components/Appbar"
-import { BlogSkeleton } from "../components/BlogSkeleton"
+import { Circle } from "../components/BlogCard"
 
 export const Blog = () => {
     const { id } = useParams()
@@ -11,15 +11,48 @@ export const Blog = () => {
     })
     if (loading || !blog) {
         return <div>
-            <Appbar />
-            <div className="flex flex-row justify-center px-100">
-                <div className="pb-10 ">
-                    <BlogSkeleton />
-                </div>
-                <div>
-                    <BlogSkeleton />
+            <div>
+                <Appbar />
+                <div role="status" className="flex justify-center animate-pulse">
+                    <div className="grid grid-cols-1 md:grid-cols-12 gap-4 px-4 md:px-20 w-full pt-12 max-w-screen-xl">
+                        <div className="md:col-span-8">
+                            <div className="text-3xl md:text-5xl font-extrabold">
+                                <div className="h-8 bg-gray-200 rounded-full mb-2.5"></div>
+                                <div className="h-8 w-62 bg-gray-200 rounded-full mb-2.5"></div>
+                                <div className="h-6 w-56 bg-gray-200 rounded-full mb-2.5"></div>
+                                <div className="h-4 bg-gray-200 rounded-full mb-2.5"></div>
+                            </div>
+                            <div className="text-slate-500 pt-2">
+                                <div className="h-2 bg-gray-200 rounded-full mb-2.5"></div>
+                            </div>
+                            <div className="pt-4 text-base md:text-xl">
+                                <div className="h-2 bg-gray-200 rounded-full mb-2.5"></div>
+                            </div>
+                        </div>
+                        <div className="md:col-span-4">
+                            <div className="text-slate-600 text-lg rounded-lg">
+                                <div className="h-2 bg-gray-200 rounded-full mb-2.5"></div>
+                                <div className="h-2 w-40 bg-gray-200 rounded-full mb-2.5"></div>
+                            </div>
+                            <div className="flex flex-col md:flex-row items-start md:items-center w-full">
+                                <div className="p-6 justify-center hidden md:visible ">
+                                    <Circle />
+                                </div>
+                                <div className="md:pl-4">
+                                    <div className="text-xl text-slate-600 font-bold ">
+                                        <div className="h-2 w-56 bg-gray-200 rounded-full mb-2.5"></div>
+                                        <div className="h-2 w-40 bg-gray-200 rounded-full mb-2.5"></div>
+                                    </div>
+                                    <div className="text-slate-500 pb-10 bottom">
+                                        <div className="h-2 bg-gray-200 rounded-full mb-2.5"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
+
 
         </div>
     }
